e77cf2f27301a73cfea73d7dcfc5c66ce21115ce
marcelkny <schleicher47@googlemail.com>
2024-05-17 08:09:39 +0200

    added button for backward navigation

--- src/components/layout/HeaderBackNavigation.tsx
+++ src/components/layout/HeaderBackNavigation.tsx
@@ -1,26 +1,32 @@
 import { useNavigate } from "react-router-dom";
 import { useCurrentNavigation } from "../../context/NavigationContext";
 import { useCallback } from "react";
+import ArrowLeftIcon from "../icons/ArrowLeftIcon";
 
 export function HeaderBackNavigation({ target_id, title }: { target_id: string | null; title: string | null }) {
     const navigate = useNavigate();
     const navInfo = useCurrentNavigation();
     const navTo = useCallback(
         (target: string | null) => {
-            const navTarget = target && target === null || target === "" ? "/" : `/${navInfo.return_id}`;
+            const navTarget = (target && target === null) || target === "" ? "/" : `/${navInfo.return_id}`;
             console.log("navTarget: ", target);
             navigate(navTarget);
         },
         [navigate]
     );
     return (
-        <div
-            className="flex items-center py-2 mr-2 hover:cursor-pointer"
-            onClick={() => {
-                navTo(navInfo.return_id);
-            }}
-        >
-            <div className="text-lg">{navInfo.title}</div>
+        <div className="flex items-center py-2 mr-2">
+            {(navInfo.return_id && navInfo.return_id === null) || navInfo.return_id === "" ? (
+                <div
+                    className="pr-4 hover:cursor-pointer"
+                    onClick={() => {
+                        navTo(navInfo.return_id);
+                    }}
+                >
+                    <ArrowLeftIcon className="w-8 h-8" />
+                </div>
+            ) : undefined}
+            <div className="text-2xl">{navInfo.title}</div>
         </div>
     );
 }
