d4b396f2e1df3d8e783cf5241c1a8a2e8c89278a
marcelkny <schleicher47@googlemail.com>
2024-05-21 08:01:46 +0200

    config updates & docker first steps

--- src/config/backend.config.ts
+++ src/config/backend.config.ts
@@ -1,5 +1,5 @@
 // Replace in productive environment
-export const BACKEND_URL="http://localhost:8091"
+export const BACKEND_URL="http://127.0.0.1:8091"
 export const DB_USER="pocket_user"
 export const DB_PASSWORD="J-Wkd3y216Bie8T"
 
